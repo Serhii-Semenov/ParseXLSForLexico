f455f0f142a9fafd0437fe31095c1a5a7f92f6ec
Serhii-Semenov <semenov!@mksat.net>
2017-07-26 00:27:03 +0300

    the latest commit

--- index.php
+++ index.php
@@ -12,23 +12,34 @@ spl_autoload_register (
     }
 );
 
-// $e = new ResultCreator( dirname( __FILE__) . '/data/iBasis.xls');
- $e = new ResultCreator(dirname(__FILE__).'/data/iBasisLightErr.xls');
-// $e = new ResultCreator(dirname(__FILE__).'/data/iBasisLight.xls');
+// За получение двух массивов с правильными и неправильными данными отвечает class ResultCreator;
+// путь к файлу передаем в конструктор
 
+ $e = new ResultCreator( dirname( __FILE__) . '/data/iBasis.xls');
+// $e = new ResultCreator(dirname(__FILE__).'/data/iBasisLightErr.xls');
+// $e = new ResultCreator(dirname(__FILE__).'/data/iBasisLight.xls');
+// $e = new ResultCreator(dirname(__FILE__).'/data/Book1.xls');
+// $e = new ResultCreator(dirname(__FILE__).'/data/Book2.xls');
+
+// выдаст ошибку на несопадение с именами по рабочим листам
 if ($e->ErrorSheetsInFile)
 {
     echo 'Sheets error!';
     exit(1);
 }
 
-while (!$e->LoadComplite)
-    sleep (3);
+// ждем загрузки и окончания маппинга файла в массивы
+// while (!$e->LoadComplite)
+//     sleep (3);
 
+// в случае невалидности выдаст сообщение
 if ($e->Error) echo '<p style="color: red"><b>Файд "невалидный"</b></p>';
 
+// сначала распечатаем валидный массив
 $e->PrintArrCoorect ();
 echo '<hr>';
+
+// рапечатыем НЕвалидный массив, если таковой существует
 $e->PrintArrIncorrect ();
 
 exit();
